44d73e700c87ecddf3a2a811846a8fd92a8e545c
dofha <shokipsh@gmail.com>
2023-03-06 23:54:52 +0100

    enable icons in lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,7 +2,6 @@ return {
   "nvim-lualine/lualine.nvim",
   opts = {
     options = {
-      icons_enabled = false,
       theme = "moonfly",
       component_separators = "|",
       section_separators = "",
